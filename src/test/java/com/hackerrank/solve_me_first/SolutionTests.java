12dc0daa9ed57e137bc0a64d01966665cbc78e27
gowthamvarma <gowthamvarma.pec@gmail.com>
2020-04-24 22:42:48 +0530

    initial checkin

---
+++ src/test/java/com/hackerrank/solve_me_first/SolutionTests.java
@@ -0,0 +1,13 @@
+package com.hackerrank.solve_me_first;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class SolutionTests {
+
+	@Test
+	public void twoSum() {
+		assertEquals(5, Solution.solveMeFirst(2,3));
+	}
+}
